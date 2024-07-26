a8a6a6908a0332c4e0fdc3e848da3f12db19a0b6
SonuKumar <djsonukumarkushwaha@gmail.com>
2024-07-26 08:19:15 +0530

    updated here

---
+++ src/module1.mjs
@@ -0,0 +1,7 @@
+const a= "rohan";
+const b = "rahul";
+const c="hritik";
+export default b;
+export {a};
+export {c};
+
---
+++ src/moudle.mjs
@@ -0,0 +1,4 @@
+import x, {a,c} from './module1.mjs'
+console.log(x);
+console.log(a);
+console.log(c);
