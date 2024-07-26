bf49861d1520a40f12acb07c840db3a271ab41e5
SonuKumar <djsonukumarkushwaha@gmail.com>
2024-07-26 21:06:42 +0530

    comment added to describe the sepration

--- src/module1.mjs
+++ src/module1.mjs
@@ -4,4 +4,4 @@ const c="hritik";
 export default b;
 export {a};
 export {c};
-
+// this is a testing module added to test importion of modules in other.
--- src/moudle.mjs
+++ src/moudle.mjs
@@ -2,3 +2,4 @@ import x, {a,c} from './module1.mjs'
 console.log(x);
 console.log(a);
 console.log(c);
+// this is a testing module added to test importion of modules in other.
