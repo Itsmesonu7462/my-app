5a5832d319f5702bc8f7d37574e3060fba52213e
SonuKumar <djsonukumarkushwaha@gmail.com>
2024-07-28 08:26:44 +0530

    added and upgraded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
+import About from './components/About';
 function App() {
   return (
     <><  NavBar title="SarthITECH" aboutText="about SarthiI" />
-    <div className="container" ><TextForm heading="how is the form" /></div></>
+    {/* <div className="container" ><TextForm heading="how is the form" /></div> */}
+  <div className="container">  <About/></div>
+    </>
 
   
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types'
 
 export default function NavBar(props) {
   return (
-    <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+ <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-dark bg-dark">
     <div className="container-fluid">
       <a className="navbar-brand" href="/">{props.title}</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,20 +18,15 @@ export default function NavBar(props) {
             <a className="nav-link" href="/">{props.aboutText}</a>
           </li>
         </ul>
-        <form className="d-flex" role="search">
+        <form className="d-flex mx-5" role="search">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
           <button className="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
     </div>
   </nav>
-    </>
   )
 }
 NavBar.PropType = {title:PropTypes.string.isRequired,
                    aboutText: PropTypes.string.isRequired
 };
-// NavBar.defaultProps={
-//     title: "set title here",
-//     aboutText: "about text here"
-// };
