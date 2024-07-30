5fb8c2196625b6b81a3e1ba2fb4c41a8dc851de8
SonuKumar <djsonukumarkushwaha@gmail.com>
2024-07-30 20:59:03 +0530

    NavBar.js and textForm.js modified and new button added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar(props) {
   return (
  <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-dark bg-dark">
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">{props.title}</a>
+      <a className="navbar-brand" href="/">SarthITECH</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -15,7 +15,7 @@ export default function NavBar(props) {
             <a className="nav-link active" aria-current="page" href="/">HOME</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">{props.aboutText}</a>
+            <a className="nav-link" href="/">about us</a>
           </li>
         </ul>
         <form className="d-flex mx-5" role="search">
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,36 +1,41 @@
 import React, {useState} from 'react'
-let heading= "hello"
+let heading= 'hello'
 export default function TexthtmlForm(props) {
 const handleclick=()=>{
-    console.log("uppper case is clicked" + text);
+    console.log('uppper case is clicked' + text);
     let newText =text.toUpperCase();
     setText(newText);
 }
 const handleOnChange=(event)=>{
-    console.log("on change");
+    console.log('on change');
     setText(event.target.value);
 }
 const handleloClick=()=>{
   let newText2 = text.toLowerCase();
   setText(newText2);
 }
-    const [text, setText ] = useState("enter text here");
+    const [text, setText ] = useState('enter text here');
+    const removeExtraSpc=()=>{
+      let NewText = text.split(/[' ']+/);
+      setText(NewText.join(' '));
+    }
   return (
    <> <div>
     <form>
-  <div className="container my-3">
-    <label htmlFor="MyMail" className="htmlForm-label"><h1>{heading}</h1> </label> <br />
-    <input type="textArea" className="text" value= {text } onChange={handleOnChange} />
+  <div className='container my-3'>
+    <label htmlFor='MyMail' className='htmlForm-label'><h1>{heading}</h1> </label> <br />
+    <input type='textArea' className='text' value= {text } onChange={handleOnChange} />
    
   </div>
-  <button className="btn btn-primary mx-2" onClick ={handleclick} >UPPER CASE</button> 
-  <button className="btn btn-primary mx-2" onClick ={handleloClick} >LOWER CASE</button> 
+  <button className='btn btn-primary mx-2' onClick ={handleclick} >UPPER CASE</button> 
+  <button className='btn btn-primary mx-2' onClick ={handleloClick} >LOWER CASE</button> 
+  <button className='btn btn-primary mx-2' onClick ={removeExtraSpc} >REMOVE EXTRA SPACE</button> 
 </form>
 </div>
-<div className="container my-4">
+<div className='container my-4'>
   <h1>text summary</h1>
-  <p>{text.length} characters in it including space and {text.split(" ").length} words in it</p>
-  <p>{0.008 * text.split(" ").length} minutes read. </p>
+  <p>{text.length} characters in it including space and {text.split(' ').length} words in it</p>
+  <p>{0.008 * text.split(' ').length} minutes read. </p>
   <h2>preview</h2>
   <p>{text}</p>
 </div>
